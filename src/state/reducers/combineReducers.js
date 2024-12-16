8b61d7a43b0862ce6f8d4a0a3288ac2aeec59d96
MehakGenboots <mehak@genboot.com>
2024-12-16 13:22:30 +0530

    login iniate

--- src/state/reducers/combineReducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import userReducer from './userReducer';
-import showPasswordReducer from './showpassword';
-const rootReducer = combineReducers({
-    user: userReducer,
-    showPassword: showPasswordReducer,
-});
-export default rootReducer;
--- src/state/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore, combineReducers} from 'redux'
-import userReducer from './reducers/userReducer'; 
-const rootReducer = combineReducers({
-  user: userReducer,
-});
-const Store = createStore(rootReducer);
-export default Store;
